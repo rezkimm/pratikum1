325dfce1ab077b39caea0aadbd3a2a28750a024f
rezkimm <rizki.snutz@gmail.com>
2016-08-29 22:00:11 +0700

    Add files via upload

---
+++ modul_arduino_4211501002.ino
@@ -0,0 +1,17 @@
+int pin = 13;
+
+void setup() {
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(pin, 1);
+  delay(200);
+  digitalWrite(pin, 0);
+  delay(200);
+  digitalWrite(pin, 1);
+  delay(1500);
+  digitalWrite(pin, 0);
+  delay(2000);
+}
+
